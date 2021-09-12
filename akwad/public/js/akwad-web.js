8f52c3ba6d06467514f8edca470f9fbe88f097fd
Santoshbb <santosh.baburao@gmail.com>
2021-09-12 16:26:04 +0300

    1st commit after app created

---
+++ akwad/public/js/akwad-web.js
@@ -0,0 +1,3 @@
+$(".footer-powered").addClass("hide");
+$( ".login-content .page-card-head > h4 " ).replaceWith( "<h4>Login to Akwad ERP</h4>" );
+$(".app-logo").attr('src', 'assets/akwad/images/akwad-logo-symbol.svg');
