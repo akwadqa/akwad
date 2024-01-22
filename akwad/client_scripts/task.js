dded6d39c760d31738acb42d764126eab8a4eadc
Alaa Hariri <alaa.hariri@outlook.com>
2024-01-22 11:27:24 +0300

    Delete akwad/client_scripts/task.js

--- akwad/client_scripts/task.js
+++
@@ -1,8 +0,0 @@
-frappe.ui.form.on('Task', {
-	validate(frm) {
-		frm.doc.actual_time = frm.doc.expected_time;
-	},
-	refresh(frm) {
-		frm.add_custom_button(frm.doc.workflow_state).css('background-color', "#FCC50A");
-	}
-});
